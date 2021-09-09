f4584105cd046b6ab08063aa4bae484c0e159117
nizzie-2204 <nguyenanhtuan.220499@gmail.com>
2021-09-09 11:08:20 +0700

    CRUD exam

--- backend/src/controllers/examController.js
+++ backend/src/controllers/examController.js
@@ -6,6 +6,11 @@ exports.createExam = async (req, res, next) => {
 		let exam = new Exam({
 			...req.body,
 		})
+
+		if (!req.files || Object.keys(req.files).length === 0) {
+			res.status(404).json({ status: 'fail', message: 'No files found' })
+		}
+
 		if (req.files) {
 			let path = ''
 			req.files.forEach((file) => {
@@ -17,6 +22,8 @@ exports.createExam = async (req, res, next) => {
 			exam.examImages = path
 		}
 
+		await Exam.create(exam)
+
 		res.status(200).json({ status: 'success', data: exam })
 	} catch (error) {
 		next(error)
@@ -25,6 +32,31 @@ exports.createExam = async (req, res, next) => {
 
 exports.updateExam = async (req, res, next) => {
 	try {
+		const exam = { ...req.body }
+
+		if (req.files) {
+			let path = ''
+			req.files.forEach((file) => {
+				path = path + file.path + ','
+			})
+
+			path = path.substring(0, path.lastIndexOf(','))
+
+			exam.examImages = path
+		}
+
+		const newExam = await Exam.findByIdAndUpdate(req.params.id, exam, {
+			new: true,
+			runValidators: true,
+		})
+
+		if (!newExam) {
+			const error = new Error('Exam does not exist: ' + userId)
+			error.statusCode = 404
+			return next(error)
+		}
+
+		res.status(200).json({ status: 'success', data: newExam })
 	} catch (error) {
 		next(error)
 	}
@@ -39,6 +71,9 @@ exports.deleteExam = async (req, res, next) => {
 
 exports.getAllExams = async (req, res, next) => {
 	try {
+		const exams = await Exam.find()
+
+		res.status(200).json({ status: 'success', data: exams })
 	} catch (error) {
 		next(error)
 	}
@@ -46,6 +81,15 @@ exports.getAllExams = async (req, res, next) => {
 
 exports.getExam = async (req, res, next) => {
 	try {
+		const exam = await Exam.findById(req.params.id)
+
+		if (!exam) {
+			const error = new Error('Exam does not exist: ' + userId)
+			error.statusCode = 404
+			return next(error)
+		}
+
+		res.status(200).json({ status: 'success', data: exam })
 	} catch (error) {
 		next(error)
 	}
--- backend/src/routes/v1/examRoute.js
+++ backend/src/routes/v1/examRoute.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const upload = require('../../middlewares/uploadFiles')
 
-const { createExam } = require('../../controllers/examController')
+const {
+	createExam,
+	getExam,
+	getAllExams,
+	updateExam,
+} = require('../../controllers/examController')
 const { permit } = require('../../middlewares/permit')
 const { verifyToken } = require('../../middlewares/auth')
 const Router = express.Router()
 
 // Allow admin to use these routes verifyToken, permit('admin'),
 
-Router.route('/exams').post(
-	verifyToken,
-	permit('admin'),
-	upload.array('examImages[]'),
-	createExam
-)
-// 	.get(verifyToken, permit('admin'), getAllClasses)
+Router.route('/exams')
+	.post(verifyToken, permit('admin'), upload.array('examImages[]'), createExam)
+	.get(verifyToken, permit('admin'), getAllExams)
 
-// Router.route('/classes/:id')
-// 	.get(verifyToken, permit('admin'), getClass)
-// 	.put(verifyToken, permit('admin'), updateClass)
+Router.route('/exams/:id')
+	.get(verifyToken, permit('admin'), getExam)
+	.put(verifyToken, permit('admin'), upload.array('examImages[]'), updateExam)
 // 	.delete(verifyToken, permit('admin'), deleteClass)
 
 module.exports = Router
